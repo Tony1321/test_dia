625446384e0b04dadecbe41254b994d32fc038d6
Tony1321 <120061810+Tony1321@users.noreply.github.com>
2025-06-15 01:00:02 +0200

    Update values.js

--- access/values.js
+++ access/values.js
@@ -4,8 +4,8 @@ var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
 var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var fullname = "Арси Хакита Патала" // ФИО
+var name = "Хакита" // Имя
 
 // Паспорт
 var passport_id = "123456789" // Номер паспорта
@@ -14,11 +14,11 @@ var passport_id = "123456789" // Номер паспорта
 var kpp_id = "0123456789" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_valid_until = "01.01.2026" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "01.01.2026" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
